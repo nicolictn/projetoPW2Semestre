98db0c80a74049c178e6d2d79b6d6260d56b7e31
Nicoli Caetano <nicolictn@gmail.com>
2024-11-01 07:56:21 -0300

    add more arqs - contact and events

---
+++ js/scriptCarrossel.js
@@ -0,0 +1,20 @@
+// const imgs = document.getElementById("img");
+// const img = document.querySelectorAll("#img img");
+
+
+// let idx = 0;
+
+// function carrossel() {
+//     idx++;
+
+//     //quando terminar a quarta imagem, o carrossel volta para a imagem inicial (0).
+//     if(idx > img.length - 1) {
+//         idx = 0;
+//     }
+
+//     img.style.transform = `translateX(${-idx * 500}px)`;
+
+// }
+
+// setInterval(carrossel, 1800);
+
