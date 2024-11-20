b6e0b10d655218d22f2be8a9d151bc05a0d7a0b4
Nicoli Caetano <nicolictn@gmail.com>
2024-11-20 14:55:06 -0300

    pré-branch responsividade

--- src/js/carousel_events.js
+++ src/js/carousel_events.js
@@ -1,12 +1,13 @@
-// utiliza a variável "E" como parâmetro
+// utiliza a variável "E" como parâmetro (pega o valor do HTML)
 function direcao(e){
     //armazena na variável "direcao" do slide 01
     var direcao = document.getElementById("contentSlide");
 
-    //caso 
+    //caso o botão esquerdo tenha sido acionado, ele dá um scroll para a esquerda.
     if(e == 1) {
+        //manipula o comportamento de rolagem horizontal
         //esquerda
-
+        //variável que guardamos o container do slide  + prorpiedade que indica quantos pixels o elemento dele rolar (- 200 é esquerda)
         direcao.scrollLeft = direcao.scrollLeft - 200;
 
     } else if(e == 2) {
